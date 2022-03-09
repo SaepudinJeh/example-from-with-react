7208ccd23d92808c7efadbddffb6212456b70bce
Saepudin <agung.saepudin@ist.id>
2022-03-09 18:04:51 +0700

    add testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,28 @@
 import { render, screen } from '@testing-library/react';
 import App from '../src/App';
 
-test('renders learn react link', () => {
+test('Should render APP component', () => {
   render(<App />);
   const linkElement = screen.getByText(/contoh form/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('Should be a form label', () => {
+  render(<App />);
+
+  expect(screen.getByLabelText(/nama depan/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/nama belakang/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/kota/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/provinsi/i)).toBeInTheDocument();
+  expect(screen.getByLabelText(/kode pos/i)).toBeInTheDocument();
+
+  expect(screen.getByRole("button", { name: /Selanjutnya/i })).toBeInTheDocument()
+});
+
+test('Must be a submit button', () => {
+  render(<App />);
+  expect(screen.getByRole("button", { name: /Selanjutnya/i })).toBeInTheDocument()
 });
 
 const fullName = (firstName) =>
--- src/components/radioForm.js
+++ src/components/radioForm.js
@@ -2,11 +2,11 @@ function RadioFrom () {
   return (
     <>
       <div className='mt-5'>
-        <label>Tipe Warga negara</label>
+        <h1>Tipe Warga Negara</h1>
         <div className="flex items-center mt-2">
           <div className="form-check form-check-inline mr-4">
             <input className="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="WNI" />
-            <label className="form-check-label inline-block text-gray-800" htmlFor="inlineRadio10">WNI ( Warga Negara Indonesi )</label>
+            <label className="form-check-label inline-block text-gray-800" htmlFor="inlineRadio10">WNI ( Warga Negara Indonesia )</label>
           </div>
           <div className="form-check form-check-inline">
             <input className="form-check-input form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="WNA" />
